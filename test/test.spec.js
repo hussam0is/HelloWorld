dc30bdd69d907fbd8f62e2dcc7f4d437e164009b
hussam <hussam.is@campus.technion.ac.il>
2020-08-25 15:44:49 +0300

    Added time out

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,76 +5,58 @@ const group3 = 'group543'
 const group4 = 'group54'
 const group5 = 'group5'
 
-describe(group1, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1, 2, 5].indexOf(5), -1);
-            function fun() {
-                setTimeout(function () {
-                    console.log("1: hello world!");
-                    console.log("2: hello world!");
-                    console.log("3: hello world!");
-                }, 30000);
-            }
-            fun();
-        });
-    });
-});
-describe(group2, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group1+' ID:00001', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(2), -1);
         });
     });
 });
-describe(group2, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group2, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(2), -1);
+        });
+    });
+});
+describe(group2, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(44), -1);
         });
     });
 });
 
-describe(group3, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group3, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
 
     });
 });
-describe(group3, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group3, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
 
     });
 });
-describe(group4, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group4, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
 
     });
 });
-describe(group5, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-
-
-    });
-});
-
-describe(group5, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group5, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
@@ -82,9 +64,19 @@ describe(group5, function() {
     });
 });
 
-describe(group5, function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe(group5, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+
+
+    });
+});
+
+describe(group5, function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
